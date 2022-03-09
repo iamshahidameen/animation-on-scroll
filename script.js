e66c9d6c828dc15c6fba88cfb28d4115e50f0af5
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-03-10 01:20:11 +0500

    With the Help of window.innerHeight and getBoundingClientRect  have used addEventListener scroll method to animate boxes when reaches certain position

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("Hello, World!");
+const boxes = document.querySelectorAll(".box");
+
+let winLowerBottom = window.innerHeight / 5 * 5;
+
+window.addEventListener('scroll', () => {
+    boxes.forEach((box) => {
+        let boxTop = box.getBoundingClientRect();
+        if(boxTop.top < winLowerBottom) {
+            box.classList.add("show");
+        } else {
+            box.classList.remove("show");
+        }
+    })
+})
+
